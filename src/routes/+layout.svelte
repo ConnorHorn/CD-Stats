cd584c12dde0be51640ce42c22db9873c4caf3b0
Connor H <Connorhornet@gmail.com>
2023-12-07 23:27:38 -0800

    undoing that

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
 	import '../app.postcss';
 	import { initializeStores, Toast } from '@skeletonlabs/skeleton';
-    import { Analytics } from '@vercel/analytics/react';
 
 
-    // Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
@@ -15,5 +13,4 @@
 
 <Toast />
 
-<Analytics />
 
