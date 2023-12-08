17d0b55726cc36996d240c3dbe5bd9bceaa605cf
Connor H <Connorhornet@gmail.com>
2023-12-07 23:13:44 -0800

    Added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.postcss';
 	import { initializeStores, Toast } from '@skeletonlabs/skeleton';
+    import { Analytics } from '@vercel/analytics/react';
 
-	// Floating UI for Popups
+
+    // Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
@@ -12,3 +14,6 @@
 <slot />
 
 <Toast />
+
+<Analytics />
+
