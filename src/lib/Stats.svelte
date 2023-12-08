4861b3a4aa4b8ca2d58ba56f4e9ae7d86e54ab8f
Connor H <Connorhornet@gmail.com>
2023-12-07 22:09:29 -0800

    Got rid of download icon. I dont think it should be downloading ideally

--- src/lib/Stats.svelte
+++ src/lib/Stats.svelte
@@ -193,7 +193,7 @@
 			</div>
 
 			<!-- Buttons container -->
-			<div class="mt-1 flex justify-center gap-4">
+			<div class="mt-1 flex justify-center gap-2">
 				{#if !/Mobi|Android/i.test(navigator.userAgent)}
 					<button
 						type="button"
@@ -215,20 +215,20 @@
 								d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
 							/>
 						</svg>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="2.5"
-							stroke="white"
-							class="h-7 w-7 stroke-gray-700"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-							/>
-						</svg>
+<!--						<svg-->
+<!--							xmlns="http://www.w3.org/2000/svg"-->
+<!--							fill="none"-->
+<!--							viewBox="0 0 24 24"-->
+<!--							stroke-width="2.5"-->
+<!--							stroke="white"-->
+<!--							class="h-7 w-7 stroke-gray-700"-->
+<!--						>-->
+<!--							<path-->
+<!--								stroke-linecap="round"-->
+<!--								stroke-linejoin="round"-->
+<!--								d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"-->
+<!--							/>-->
+<!--						</svg>-->
 					</button>
 				{/if}
 				<button type="button" class="variant-filled-tertiary btn btn-sm" on:click={handleShare}>
